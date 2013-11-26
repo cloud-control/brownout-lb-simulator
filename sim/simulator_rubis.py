3da9a5255fc1d3c42ccbe439ac1fc92eead655aa
Cristian KLEIN <cristiklein@gmail.com>
2013-11-26 14:32:42 +0100

    First working version

--- sim/simulator_rubis.py
+++ sim/simulator_rubis.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 from __future__ import division, print_function
 
-from collection import defaultdict
+from collections import defaultdict
 import heapq
 import Queue
 import random
@@ -22,45 +23,59 @@ class Simulator:
 		self.events = []
 		self.now = 0
 
-	def add(delay, what):
+	def add(self, delay, what):
 		heapq.heappush(self.events, (self.now + delay, what))
 
-	def run(until = 100):
+	def run(self, until = 100):
 		while self.events:
 			self.now, event = heapq.heappop(self.events)
 			if self.now > until:
 				return
 			event()
 
-	def log(issuer, message, *args):
-		print(self.now, str(issuer), message.format(*args))
+	def log(self, issuer, message, *args):
+		print(self.now, str(issuer), message.format(*args), file = sys.stderr)
+
+class Request:
+	lastRequestId = 1
+	
+	def __init__(self):
+		self.requestId = Request.lastRequestId
+		Request.lastRequestId += 1
+
+	def __str__(self):
+		return str(self.requestId)
 
 class Server:
-	lastServertId = 1
+	lastServerId = 1
 
 	def __init__(self, sim, **kwargs):
 		self.serviceTimeY = 0.02 # service time with recommender system
 		self.serviceTimeN = 0.001 # and without it
 		self.controlPeriod = 1 # second
-		self.name = 'server' + str(lastServerId)
-		lastServerId += 1
+		self.setPoint = 1 # second
+		self.name = 'server' + str(Server.lastServerId)
+		Server.lastServerId += 1
 
 		self.sim = sim
 		self.theta = kwargs.get('initialTheta', 0.5)
-		self.add(0, lambda: self.runControlLoop())
+		self.sim.add(0, lambda: self.runControlLoop())
 		self.latestLatencies = []
 		self.requestQueue = []
 
 	def runControlLoop(self):
-		c_est = max(self.latestLatencies) / self.theta # very rough estimate
-		pole = 0.9
-		error = set_point - max(self.latestLatencies)
-		self.theta += (1/c_est) * (1 - pole) * error
+		if self.latestLatencies:
+			c_est = max(self.latestLatencies) / self.theta # very rough estimate
+			pole = 0.9
+			error = self.setPoint - max(self.latestLatencies)
+			self.theta += (1/c_est) * (1 - pole) * error
 
-		# saturation, it's a probability
-		self.theta = min(max(self.theta, 0.0), 1.0)
+			# saturation, it's a probability
+			self.theta = min(max(self.theta, 0.0), 1.0)
 
-		self.latestLatencies = []
+			self.latestLatencies = []
+
+		# Re-run later
 		self.sim.add(self.controlPeriod, lambda: self.runControlLoop())
 
 	def request(self, request):
@@ -73,14 +88,14 @@ class Server:
 	def serve(self, request):
 		executeRecommender = random.random() <= self.theta
 		request.theta = self.theta
-		computationTime =
+		computationTime = \
 			self.serviceTimeY if executeRecommender else self.serviceTimeN
 		self.sim.add(computationTime, lambda: self.onCompleted(request))
 
 	def onCompleted(self, request):
 		request.completion = self.sim.now
 		self.latestLatencies.append(request.completion - request.arrival)
-		self.request.onCompleted()
+		request.onCompleted()
 		if self.requestQueue:
 			request = self.requestQueue.pop()
 			self.sim.add(0, lambda: self.serve(request))
@@ -95,7 +110,7 @@ class LoadBalancer:
 		self.weights = []
 		self.lastThetas = []
 		
-		self.add(0, self.runControlLoop)
+		self.sim.add(0, self.runControlLoop)
 
 	def addBackend(self, backend):
 		self.backends.append(backend)
@@ -104,45 +119,48 @@ class LoadBalancer:
 		self.weights = [ 1.0 / len(self.backends) ] * len(self.backends)
 
 	def request(self, request):
+		self.sim.log(self, "Got request {0}", request)
 		generatedWeight = random.random()
-		request.chosenBackend = weightedChoice(zip(self.backend, self.weight))
 		request.arrival = self.sim.now
-		newRequest = object()
+		newRequest = Request()
+		newRequest.chosenBackendIndex = weightedChoice(zip(range(0, len(self.backends)), self.weights))
 		newRequest.originalRequest = request
-		newRequest.onCompleted = self.onCompleted(request)
-		self.backend[chosenBackend].request(request)
-		
-		self.sim.log(self, "Directed request to {0}", chosenBackend)
+		newRequest.onCompleted = lambda: self.onCompleted(newRequest)
+		self.sim.log(self, "Directed request to {0}", newRequest.chosenBackendIndex)
+		self.backends[newRequest.chosenBackendIndex].request(newRequest)
 
 	def onCompleted(self, request):
-		self.lastThetas[request.chosenBackend]
+		self.lastThetas[request.chosenBackendIndex]
 		request.completion = self.sim.now
 		request.originalRequest.onCompleted()
 
 	def runControlLoop(self):
 		sumOfThetas = sum(self.lastThetas)
-		self.weights = map(lambda x: x / sumOfThetas, self.thetas)
+		self.weights = map(lambda x: x / sumOfThetas, self.lastThetas)
 		self.sim.add(self.controlPeriod, self.runControlLoop)
+
+	def __str__(self):
+		return "lb"
 
 class ClosedLoopClient:
 	lastClientId = 1
 
 	def __init__(self, sim, server, **kwargs):
 		self.averageThinkTime = kwargs.get('thinkTime', 1)
-		self.name = 'client' + str(lastClientId)
-		lastClientId += 1
+		self.name = 'client' + str(ClosedLoopClient.lastClientId)
+		ClosedLoopClient.lastClientId += 1
 
 		self.sim = sim
 		self.server = server
-		self.add(0, self.issueRequest)
+		self.sim.add(0, self.issueRequest)
 
 	def issueRequest(self):
-		request = object()
-		request.onCompleted = self.onCompleted
+		request = Request()
+		request.onCompleted = lambda: self.onCompleted(request)
+		self.sim.log(self, "Requested {0}", request)
 		self.server.request(request)
-		self.sim.log(self, "Requested")
 
-	def onCompleted(self, request);
+	def onCompleted(self, request):
 		thinkTime = random.expovariate(1.0 / self.averageThinkTime)
 		self.sim.add(thinkTime, self.issueRequest)
 		self.sim.log(self, "Thinking for {0}", thinkTime)
@@ -151,7 +169,7 @@ class ClosedLoopClient:
 		return self.name
 
 if __name__ == "__main__":
-	numClients = 10
+	numClients = 1
 
 	sim = Simulator()
 	server1 = Server(sim)
