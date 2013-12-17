73798b9ee9955bae75625cd25fed61aa9c88f4d8
Cristian KLEIN <cristiklein@gmail.com>
2013-12-17 09:30:16 +0100

    Plot derivative of requests

--- sim/plotresults.py
+++ sim/plotresults.py
@@ -16,6 +16,13 @@ def aggregate(a, interval = 10, func = avg):
 			ret[i,j] = func(a[i:i+interval,j])
 	return ret
 
+def derivate(a):
+	ret = np.zeros(a.shape)
+	for i in range(1, a.shape[0]):
+		for j in range(0, a.shape[1]):
+			ret[i,j] = a[i,j] - a[i-1,j]
+	return ret
+
 def main():
 	replicas = 0
 	for f in os.listdir('.'):
@@ -30,13 +37,15 @@ def main():
 	dimmers = data[:,replicas+1:2*replicas+1]
 	avg_latencies = data[:,2*replicas+1:3*replicas+1]
 	max_latencies = data[:,3*replicas+1:4*replicas+1]
-	total_requests = data[:,4*replicas+1]
-	optional_requests = data[:,4*replicas+2]
+	total_requests = data[:,4*replicas+1:4*replicas+2]
+	optional_requests = data[:,4*replicas+2:4*replicas+3]
 	effective_weights = data[:,4*replicas+3:5*replicas+3]
 
 	# make plots more readable
 	max_latencies = aggregate(max_latencies, func = max)
 	effective_weights = aggregate(effective_weights)
+	diff_total_requests = aggregate(derivate(total_requests))
+	diff_optional_requests = aggregate(derivate(optional_requests))
 	
 	subplot(3, 2, 1)
 	plot(times, weights)
@@ -48,8 +57,10 @@ def main():
 	plot(times, max_latencies)
 	title("Max latencies")
 	subplot(3, 2, 4)
-	plot(times, optional_requests/total_requests)
-	title("Percentage recommendations")
+	plot(times, diff_total_requests, label = "total")
+	plot(times, diff_optional_requests, label = "w/ optional")
+	legend()
+	title("Requests served over time")
 	subplot(3, 2, 5)
 	plot(times, effective_weights)
 	title("Effective weights")
