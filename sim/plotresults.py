0f11776b61e3da9cafdaa3eaa80c0e4c3dd6f0e6
Cristian KLEIN <cristiklein@gmail.com>
2013-12-17 09:05:50 +0100

    Fix input indexes

--- sim/plotresults.py
+++ sim/plotresults.py
@@ -14,13 +14,13 @@ def main():
 
 	# parse data
 	times = data[:,0]
-	weights = data[:,1:replicas]
-	dimmers = data[:,replicas+1:2*replicas]
-	avg_latencies = data[:,2*replicas+1:3*replicas]
-	max_latencies = data[:,3*replicas+1:4*replicas]
+	weights = data[:,1:replicas+1]
+	dimmers = data[:,replicas+1:2*replicas+1]
+	avg_latencies = data[:,2*replicas+1:3*replicas+1]
+	max_latencies = data[:,3*replicas+1:4*replicas+1]
 	total_requests = data[:,4*replicas+1]
 	optional_requests = data[:,4*replicas+2]
-	effective_weights = data[:,4*replicas+2:5*replicas+1]
+	effective_weights = data[:,4*replicas+3:5*replicas+3]
 
 	figure(1)
 	plot(times, weights)
