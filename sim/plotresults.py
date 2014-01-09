2255c2d2a1243c975ba0cfcc4a9c62c6f620441d
Martina Maggio <maggio.martina@gmail.com>
2014-01-09 16:21:04 +0100

    Plotting all the data contained in the results folder

--- sim/plotresults.py
+++ sim/plotresults.py
@@ -2,7 +2,9 @@
 import fnmatch
 import os
 import numpy as np
-from pylab import *
+#from pylab import *
+import matplotlib.pyplot as plt
+
 
 def avg(a):
 	if len(a) == 0:
@@ -24,48 +26,55 @@ def derivate(a):
 	return ret
 
 def main():
-	replicas = 0
-	for f in os.listdir('.'):
-		if fnmatch.fnmatch(f, 'sim-lb.csv'):
-			data = np.genfromtxt(f, dtype=float, delimiter=',', invalid_raise=False) 
-		if fnmatch.fnmatch(f, 'sim-server*.csv'):
-			replicas += 1
 
-	# parse data
-	times = data[:,0]
-	weights = data[:,1:replicas+1]
-	dimmers = data[:,replicas+1:2*replicas+1]
-	avg_latencies = data[:,2*replicas+1:3*replicas+1]
-	max_latencies = data[:,3*replicas+1:4*replicas+1]
-	total_requests = data[:,4*replicas+1:4*replicas+2]
-	optional_requests = data[:,4*replicas+2:4*replicas+3]
-	effective_weights = data[:,4*replicas+3:5*replicas+3]
+	for alg in os.listdir('results'):
 
-	# make plots more readable
-	max_latencies = aggregate(max_latencies, func = max)
-	effective_weights = aggregate(effective_weights)
-	diff_total_requests = aggregate(derivate(total_requests))
-	diff_optional_requests = aggregate(derivate(optional_requests))
-	
-	subplot(3, 2, 1)
-	plot(times, weights)
-	title("Weights")
-	subplot(3, 2, 2)
-	plot(times, dimmers)
-	title("Dimmers")
-	subplot(3, 2, 3)
-	plot(times, max_latencies)
-	title("Max latencies")
-	subplot(3, 2, 4)
-	plot(times, diff_total_requests, label = "total")
-	plot(times, diff_optional_requests, label = "w/ optional")
-	legend()
-	title("Requests served over time")
-	subplot(3, 2, 5)
-	plot(times, effective_weights)
-	title("Effective weights")
-	show()
+		plt.ioff()
+		replicas = 0
+		for f in os.listdir('./results/' + alg):
+			datafile = './results/' + alg + "/" + f
+			if fnmatch.fnmatch(f, 'sim-lb.csv'):
+				data = np.genfromtxt(datafile, dtype=float, delimiter=',', invalid_raise=False) 
+			if fnmatch.fnmatch(f, 'sim-server*.csv'):
+				replicas += 1
 
+		# parse data
+		times = data[:,0]
+		weights = data[:,1:replicas+1]
+		dimmers = data[:,replicas+1:2*replicas+1]
+		avg_latencies = data[:,2*replicas+1:3*replicas+1]
+		max_latencies = data[:,3*replicas+1:4*replicas+1]
+		total_requests = data[:,4*replicas+1:4*replicas+2]
+		optional_requests = data[:,4*replicas+2:4*replicas+3]
+		effective_weights = data[:,4*replicas+3:5*replicas+3]
+
+		# make plots more readable
+		max_latencies = aggregate(max_latencies, func = max)
+		effective_weights = aggregate(effective_weights)
+		diff_total_requests = aggregate(derivate(total_requests))
+		diff_optional_requests = aggregate(derivate(optional_requests))
+
+		f = plt.figure()
+		f.suptitle(alg, fontsize=20)
+		plt.subplot(3, 2, 1)
+		plt.plot(times, weights)
+		plt.title("Weights")
+		plt.subplot(3, 2, 2)
+		plt.plot(times, dimmers)
+		plt.title("Dimmers")
+		plt.subplot(3, 2, 3)
+		plt.plot(times, max_latencies)
+		plt.title("Max latencies")
+		plt.subplot(3, 2, 4)
+		plt.plot(times, diff_total_requests, label = "total")
+		plt.plot(times, diff_optional_requests, label = "w/ optional")
+		plt.legend()
+		plt.title("Requests served over time")
+		plt.subplot(3, 2, 5)
+		plt.plot(times, effective_weights)
+		plt.title("Effective weights")
+
+	plt.show()
 
 if __name__ == "__main__":
 	main()
