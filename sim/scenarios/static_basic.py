e709c96546c6c808c646ad60b42b5c9a7909b490
apapadopoulos <alessandro.papadopoulos@gmail.com>
2014-01-27 16:12:43 +0100

    Minor modifications to theta-diff-plus

---
+++ sim/scenarios/static_basic.py
@@ -0,0 +1,11 @@
+# Scenario that we have been using almost since the beginning
+
+addServer(y = 0.07     , n = 0.001     )
+addServer(y = 0.07 *  2, n = 0.001 *  2)
+addServer(y = 0.07 *  3, n = 0.001 *  3)
+addServer(y = 0.07 * 10, n = 0.001 * 50)
+addServer(y = 0.07 * 10, n = 0.001 * 50)
+
+addClients(at =    0, n = 50)
+
+endOfSimulation(at = 5000)
