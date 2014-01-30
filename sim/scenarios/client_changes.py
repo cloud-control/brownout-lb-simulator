1b4053426a698504b498a5f609f36348be2c7879
Martina Maggio <maggio.martina@gmail.com>
2014-01-30 17:54:51 +0100

    First scenario of the client changes series

---
+++ sim/scenarios/client_changes.py
@@ -0,0 +1,28 @@
+# four fast replicas
+addServer(y = 0.05     , n = 0.005     )
+addServer(y = 0.05     , n = 0.005     )
+addServer(y = 0.05     , n = 0.005     )
+addServer(y = 0.05     , n = 0.005     )
+
+# four slow replicas
+addServer(y = 0.05 * 10, n = 0.005 * 5 )
+addServer(y = 0.05 * 10, n = 0.005 * 5 )
+addServer(y = 0.05 * 10, n = 0.005 * 5 )
+addServer(y = 0.05 * 10, n = 0.005 * 5 )
+
+# two super-slow replicas
+addServer(y = 0.05 * 50, n = 0.005 * 10)
+addServer(y = 0.05 * 50, n = 0.005 * 10)
+
+addClients(at =    0, n = 50 )
+addClients(at = 1000, n = 50 )
+addClients(at = 2000, n = 100)
+addClients(at = 3000, n = 50 )
+delClients(at = 4000, n = 75 )
+delClients(at = 5000, n = 75 )
+delClients(at = 6000, n = 100)
+addClients(at = 7000, n = 50 )
+delClients(at = 8000, n = 25 )
+addClients(at = 9000, n = 25 )
+
+endOfSimulation(at = 10000)
