2f872133bd612ed155ef6ada713ce8498e482c05
apapadopoulos <alessandro.papadopoulos@gmail.com>
2014-02-03 14:24:52 +0100

    Modified client changes 3 for paper case study

--- sim/scenarios/client_changes_3.py
+++ sim/scenarios/client_changes_3.py
@@ -2,21 +2,16 @@
 addServer(y = 0.05     , n = 0.005     )
 
 # one slow replicas
-addServer(y = 0.05 * 10, n = 0.005 * 5 )
+addServer(y = 0.05 * 5, n = 0.005 * 5 )
 
-# one super-slow replicas
-addServer(y = 0.05 * 50, n = 0.005 * 10)
-addServer(y = 0.05 * 50, n = 0.005 * 10)
+# two super-slow replicas
+addServer(y = 0.05 * 10, n = 0.005 * 10)
+addServer(y = 0.05 * 10, n = 0.005 * 10)
 
-addClients(at =    0, n = 10 )
-delClients(at = 1000, n =  5 )
-addClients(at = 2000, n = 10 )
-addClients(at = 3000, n =  5 )
-delClients(at = 4000, n = 15 )
-addClients(at = 5000, n = 10 )
-delClients(at = 6000, n =  5 )
-addClients(at = 7000, n = 20 )
-delClients(at = 8000, n = 10 )
-addClients(at = 9000, n =  5 )
+addClients(at =   0, n = 50 )
+delClients(at = 200, n = 10 )
+addClients(at = 400, n = 25 )
+addClients(at = 600, n = 25 )
+delClients(at = 800, n = 40 )
 
-endOfSimulation(at = 10000)
+endOfSimulation(at = 1000)
