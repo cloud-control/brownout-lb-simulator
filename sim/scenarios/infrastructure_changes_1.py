7015db3b428c53d5b963c098be6f68b63838250c
Martina Maggio <maggio.martina@gmail.com>
2014-02-03 16:47:34 +0100

    Kind of first draft ready

--- sim/scenarios/infrastructure_changes_1.py
+++ sim/scenarios/infrastructure_changes_1.py
@@ -6,11 +6,10 @@ addServer(y = 0.07 *  2, n = 0.001 *  2)
 addServer(y = 0.07 * 10, n = 0.001 * 10)
 addServer(y = 0.07 * 10, n = 0.001 * 10)
 
-addClients(at =    0, n = 100)
+addClients(at =    0, n = 50)
 
-changeServiceTime(at = 200, serverId = 0, y = 0.07 * 10, n = 0.001 * 10)
-changeServiceTime(at = 400, serverId = 4, y = 0.07 * 2,  n = 0.001 * 2)
-changeServiceTime(at = 600, serverId = 0, y = 0.07 * 2,  n = 0.001 * 2)
-changeServiceTime(at = 800, serverId = 0, y = 0.07 * 1,  n = 0.001 * 1)
+changeServiceTime(at = 250, serverId = 0, y = 0.07 * 5,  n = 0.001 * 5)
+changeServiceTime(at = 500, serverId = 4, y = 0.07 * 1,  n = 0.001 * 1)
+changeServiceTime(at = 750, serverId = 3, y = 0.07 * 1,  n = 0.001 * 1)
 
-endOfSimulation(at = 5000)
+endOfSimulation(at = 1000)
