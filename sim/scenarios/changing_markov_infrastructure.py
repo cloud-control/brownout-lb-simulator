324f3338553d7fcacd316bc46053b32f2beac6ed
Manfred Dellkrantz <manfred@control.lth.se>
2014-02-25 11:04:36 +0100

    Last push broke changing_markov_infrastructure.py. Now fixed.

--- sim/scenarios/changing_markov_infrastructure.py
+++ sim/scenarios/changing_markov_infrastructure.py
@@ -6,11 +6,13 @@ addServer(y = 0.07 *  3, n = 0.001 *  3)
 addServer(y = 0.07 * 10, n = 0.001 * 50)
 addServer(y = 0.07 * 10, n = 0.001 * 50)
 
-sim.markovClients = MarkovianArrivalProcess(sim,loadBalancer,rate=30)
+global markovClients
 
-setRate(1000, 60)
-setRate(2000, 40)
-setRate(4000, 80)
+markovClients = MarkovianArrivalProcess(sim,loadBalancer,rate=30)
+
+sim.add(1000, lambda: markovClients.setRate(60))
+sim.add(2000, lambda: markovClients.setRate(40))
+sim.add(4000, lambda: markovClients.setRate(80))
 
 changeServiceTime(at = 250, serverId = 0, y = 0.07 * 5,  n = 0.001 * 5)
 changeServiceTime(at = 500, serverId = 4, y = 0.07 * 1,  n = 0.001 * 1)
