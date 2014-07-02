436f50305094978c74bcf212ba082c7aeec48e9c
Cristian KLEIN <cristiklein@gmail.com>
2014-07-02 14:27:27 +0200

    Also take into account variance when doing simulations

--- simulator.py
+++ simulator.py
@@ -18,7 +18,9 @@ from SimulatorKernel import *
 
 ## @package simulator Main simulator namespace
 
-def runSimulation(arrivalRate, outDir, discipline, brownoutMethod, seed):
+def runSimulation(arrivalRate, outDir, discipline, brownoutMethod, seed,
+	timeYmu, timeYsigma,
+	timeNmu, timeNsigma):
 	with open(pathjoin(outDir, 'parameters.csv'), 'w') as f:
 		print('key,value', file = f)
 		for k,v in locals().iteritems():
@@ -27,7 +29,8 @@ def runSimulation(arrivalRate, outDir, discipline, brownoutMethod, seed):
 	sim = SimulatorKernel(outputDirectory = outDir)
 	replica = Replica(sim = sim,
 		seed = seed,
-		timeSlice = 0.01 if discipline == 'ps' else 1)
+		timeSlice = 0.01 if discipline == 'ps' else 1,
+		timeY = (timeYmu, timeYsigma), timeN = (timeNmu, timeNsigma))
 
 	brownoutProxy = BrownoutProxy(sim = sim, server = replica,
 		processorSharing = True if discipline == 'ps' else False,
@@ -71,32 +74,37 @@ def main():
 	args = parser.parse_args()
 
 	header = None
-	for seed in range(1, 10):
-		for arrivalRate in range(1, 10) + range(10, 100, 10):
-			parameters = {
-				'arrivalRate'   : arrivalRate,
-				'discipline'    : args.discipline,
-				'brownoutMethod': args.brownoutMethod,
-				'seed'          : seed,
-			}
+	for relativeDeviation in range(1, 30):
+		for seed in range(1, 10):
+			for arrivalRate in range(1, 10) + range(10, 100, 10):
+				parameters = {
+					'arrivalRate'   : arrivalRate,
+					'discipline'    : args.discipline,
+					'brownoutMethod': args.brownoutMethod,
+					'seed'          : seed,
+					'timeYmu'       : 0.07,
+					'timeYsigma'    : 0.07 * relativeDeviation / 100,
+					'timeNmu'       : 0.00067,
+					'timeNsigma'    : 0.00067 * relativeDeviation / 100,
+				}
 
-			outDir = '-'.join(
-				['results'] +
-				[v for v in parameters.itervalues() if type(v) == str] +
-				['{0}{1:04d}'.format(k, v) for k, v in parameters.iteritems() if type(v) == int]
-			)
-			try:
-				os.makedirs(outDir)
-			except:
-				pass
-			parameters['outDir'] = outDir
-			metrics = runSimulation(**parameters)
+				outDir = '-'.join(
+					['results'] +
+					[v for v in parameters.itervalues() if type(v) == str] +
+					['{0}{1:04d}'.format(k, v) for k, v in parameters.iteritems() if type(v) == int]
+				)
+				try:
+					os.makedirs(outDir)
+				except:
+					pass
+				parameters['outDir'] = outDir
+				metrics = runSimulation(**parameters)
 
-			if header is None:
-				header = sorted(parameters) + sorted(metrics)
-				print(*header, sep = ',')
-			metrics.update(parameters)
-			print(*[ metrics[k] for k in header ], sep = ',')
+				if header is None:
+					header = sorted(parameters) + sorted(metrics)
+					print(*header, sep = ',')
+				metrics.update(parameters)
+				print(*[ metrics[k] for k in header ], sep = ',')
 
 if __name__ == "__main__":
 	main()
