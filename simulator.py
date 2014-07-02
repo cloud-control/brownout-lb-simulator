331dd5299acba178a47d79ccdfdd15cb6503064f
Cristian KLEIN <cristiklein@gmail.com>
2014-07-02 13:47:13 +0200

    Separated running simulations in a new function

--- simulator.py
+++ simulator.py
@@ -7,6 +7,7 @@ from __future__ import division, print_function
 
 import argparse
 import os
+from os.path import join as pathjoin
 import sys
 import numpy as np
 
@@ -16,6 +17,36 @@ from Replica import *
 from SimulatorKernel import *
 
 ## @package simulator Main simulator namespace
+
+def runSimulation(arrivalRate, outDir, discipline, brownoutMethod, seed):
+	with open(pathjoin(outDir, 'parameters.csv'), 'w') as f:
+		print('key,value', file = f)
+		for k,v in locals().iteritems():
+			print(k, v, file = f)
+
+	sim = SimulatorKernel(outputDirectory = outDir)
+	replica = Replica(sim = sim,
+		seed = seed,
+		timeSlice = 0.01 if discipline == 'ps' else 1)
+
+	brownoutProxy = BrownoutProxy(sim = sim, server = replica,
+		processorSharing = True if discipline == 'ps' else False,
+		queueCut = True if brownoutMethod == 'queuecut' else False)
+
+	client = Client(sim = sim, server = brownoutProxy, rate = arrivalRate, seed = seed)
+
+	sim.run(until = 100)
+
+	# Report results
+	return {
+		'rtAvg' : avg(client.responseTimes),
+		'rtP95' : np.percentile(client.responseTimes, 95),
+		'rtP99' : np.percentile(client.responseTimes, 99),
+		'rtP999': np.percentile(client.responseTimes, 99.9),
+		'rtMax' : max(client.responseTimes),
+		'optionalRatio': client.numCompletedRequestsWithOptional / client.numCompletedRequests,
+		'utilization'  : replica.getActiveTime() / sim.now,
+	}
 
 ## Entry-point for simulator.
 # Setups up all entities, then runs simulation.
@@ -39,9 +70,7 @@ def main():
 
 	args = parser.parse_args()
 
-	print("#", ' '.join(sys.argv))
-	print("arrivalRate,rtAvg,rt95,rt99,rt999,rtMax,optionalRatio,utilization")
-
+	header = None
 	for arrivalRate in range(1, 10) + range(10, 100, 10):
 		outDir = 'results-{0:02d}'.format(arrivalRate)
 		try:
@@ -49,28 +78,20 @@ def main():
 		except:
 			pass
 
-		sim = SimulatorKernel(outputDirectory = outDir)
-		replica = Replica(sim = sim,
-			seed = args.seed,
-			timeSlice = 0.01 if args.discipline == 'ps' else 1)
+		parameters = {
+			'arrivalRate'   : arrivalRate,
+			'discipline'    : args.discipline,
+			'brownoutMethod': args.brownoutMethod,
+			'outDir'        : outDir,
+			'seed'          : args.seed,
+		}
+		metrics = runSimulation(**parameters)
 
-		brownoutProxy = BrownoutProxy(sim = sim, server = replica,
-			processorSharing = True if args.discipline == 'ps' else False,
-			queueCut = True if args.brownoutMethod == 'queuecut' else False)
-
-		client = Client(sim = sim, server = brownoutProxy, rate = arrivalRate, seed = args.seed)
-
-		sim.run(until = 100)
-
-		# Report results
-		rtAvg = avg(client.responseTimes)
-		rtP95 = np.percentile(client.responseTimes, 95)
-		rtP99 = np.percentile(client.responseTimes, 99)
-		rtP999 = np.percentile(client.responseTimes, 99.9)
-		rtMax = max(client.responseTimes)
-		optionalRatio = client.numCompletedRequestsWithOptional / client.numCompletedRequests
-		utilization = replica.getActiveTime() / sim.now
-		print(arrivalRate, rtAvg, rtP95, rtP99, rtP999, rtMax, optionalRatio, utilization, sep = ',')
+		if header is None:
+			header = sorted(parameters) + sorted(metrics)
+			print(*header, sep = ',')
+		metrics.update(parameters)
+		print(*[ metrics[k] for k in header ], sep = ',')
 
 if __name__ == "__main__":
 	main()
