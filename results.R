79f4a2665c8d74c4c866d073e348c5ab728e5cff
Cristian KLEIN <cristiklein@gmail.com>
2014-07-03 09:04:20 +0200

    Added some results

--- results.R
+++ results.R
@@ -1,17 +1,24 @@
-results <- read.csv("~/work/2013-brownout/brownout-lb-simulator/results-ps.csv", header=TRUE, comment.char = '#')
+if (!exists("discipline")) {
+  stop("Please set the discipline variable")
+}
+results <- read.csv(paste("results-", discipline, ".csv", sep = ''), header=TRUE, comment.char = '#')
 
 par(mfrow = c(2, 1), mar = c(4,4,2,2))
-plot(c(0, max(results$arrivalRate)), c(0, 1), type = 'n',
+plot(c(0, max(results$arrivalRate)), c(0, 1.0), type = 'n',
     xlab = 'Arrival Rate [requests / s]', ylab = '')
 points(results$arrivalRate, results$optionalRatio, pch = '+', col = 'blue')
 points(results$arrivalRate, results$rtMax, pch = '+', col = 'red')
+points(results$arrivalRate, results$utilization, pch = '+', col = 'darkgreen')
 abline(h = 0.5)
 grid()
 
-legend('topright',
-      c('optionalRatio', 'rtMax'),
+par(xpd=TRUE)
+legend(10, 1.31,
+      horiz = TRUE,
+      c('optionalRatio', 'rtMax', 'utilization'),
       pch = '+',
-      col = c('blue', 'red'))
+      col = c('blue', 'red', 'darkgreen'))
+par(xpd=FALSE)
 
 plot(
   results$timeYsigma[results$arrivalRate > 10],
