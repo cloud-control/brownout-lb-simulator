efe6c777f529c9b4bef9d4eb42aea6b7983c754e
Cristian Klein <cristiklein@gmail.com>
2016-02-09 16:30:43 +0100

    Made auto-scaler test more interesting

--- scenarios/autoscaling-support.py
+++ scenarios/autoscaling-support.py
@@ -1,14 +1,16 @@
 # Scenario to test autoscaling
 
-addServer(y = 0.07     , n = 0.001     , autoScale = False) # ensure at least one server is connected
-addServer(y = 0.07 *  2, n = 0.001 *  2, autoScale = True)
-addServer(y = 0.07 *  3, n = 0.001 *  3, autoScale = True)
-addServer(y = 0.07 * 10, n = 0.001 * 50, autoScale = True)
-addServer(y = 0.07 * 10, n = 0.001 * 50, autoScale = True)
+# TODO: Consider adding heterogenity to servers to highlight the strenghts of
+# brownout-aware load-balancing algorithms.
+addServer(y = 0.07, n = 0.001, autoScale = False) # ensure at least one server is connected
+addServer(y = 0.07, n = 0.001, autoScale = True )
+addServer(y = 0.07, n = 0.001, autoScale = True )
+addServer(y = 0.07, n = 0.001, autoScale = True )
+addServer(y = 0.07, n = 0.001, autoScale = True )
 
-setRate(at =    0, rate = 30)
-setRate(at = 1000, rate = 60)
+setRate(at =    0, rate = 10)
+setRate(at = 1000, rate = 20)
 setRate(at = 2000, rate = 40)
-setRate(at = 4000, rate = 80)
+setRate(at = 4000, rate = 10)
 
 endOfSimulation(at = 5000)
