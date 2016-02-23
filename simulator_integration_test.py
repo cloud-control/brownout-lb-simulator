3205804934836a38e3c1a132973fd9a50c0fcbae
Cristian Klein <cristiklein@gmail.com>
2016-02-23 22:11:00 +0100

    Test for invalid parameter

--- simulator_integration_test.py
+++ simulator_integration_test.py
@@ -1,4 +1,5 @@
 import mock
+from nose.tools import *
 import os
 
 from simulator import loadBalancingAlgorithms, main
@@ -7,3 +8,8 @@ def test_main():
     for algorithm in loadBalancingAlgorithms:
         with mock.patch('sys.argv', [os.getcwd() + '/simulator.py', '--algorithm', algorithm]):
             main()
+
+@raises(SystemExit)
+def test_invalid_algorithm():
+    with mock.patch('sys.argv', [os.getcwd() + '/simulator.py', '--algorithm', 'non-existant']):
+        main()
