c1786944c60af37c7b95200216d50f6992820543
Cristian Klein <cristiklein@gmail.com>
2016-02-24 10:50:21 +0100

    Added test for load-balancer

---
+++ LoadBalancer_test.py
@@ -0,0 +1,18 @@
+from mock import Mock
+
+from LoadBalancer import *
+from SimulatorKernel import *
+
+def test():
+    sim = SimulatorKernel()
+
+    server1 = Mock()
+    server2 = Mock()
+
+    lb = LoadBalancer(sim)
+    lb.addBackend(server1)
+    lb.addBackend(server2)
+
+    lb.removeBackend(server2)
+
+    assert str(lb)
