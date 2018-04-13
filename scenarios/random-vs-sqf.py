818967752563e146e8296024233a63fc2fa11aa1
Tommi Nylander <tommi.nylander@control.lth.se>
2018-04-13 14:00:22 +0200

    Small fix to random vs sqf scenario.

--- scenarios/random-vs-sqf.py
+++ scenarios/random-vs-sqf.py
@@ -7,10 +7,12 @@ addServer(at=0.0,y = speedfactor * 0.07    , n = speedfactor * 0.001    )
 
 nbrIterations = 21
 
+changeMC(at=1.0, newMC=15)
+
 for i in range(0, nbrIterations):
-    setRate(at=0 + 100 * i, rate = 400.0)
-    changeMC(at=1.0, newMC=15)
-    setRate(at =50   + 100*i, rate = 1500.0)
+    setRate(at=0 + 150 * i, rate = 400.0)
+    setRate(at =50   + 150*i, rate = 1500.0)
+    setRate(at=100 + 150 * i, rate=400.0)
 
 
-endOfSimulation(at = 100*nbrIterations)
+endOfSimulation(at = 150*nbrIterations)
