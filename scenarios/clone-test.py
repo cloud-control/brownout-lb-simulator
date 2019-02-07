02954178880d3ab70e3ddc2e3c8faeb6f8b7b8a3
Tommi Nylander <tommi.nylander@control.lth.se>
2019-02-07 17:05:27 +0100

    Added changeMC line to scenario file to set FCFS or PS behaviour for servers.

--- scenarios/clone-test.py
+++ scenarios/clone-test.py
@@ -2,7 +2,7 @@ import scipy.stats
 import numpy as np
 from base.numerical_dist import NumericalDistribution
 
-nbrServers = 12
+nbrServers = 3
 for i in range(0, nbrServers):
 
     if dist == "fromPath":
@@ -13,13 +13,15 @@ for i in range(0, nbrServers):
         except:
             raise ValueError("Distribution could not load, does it exist in scipy.stats?")
         serviceTimeDistribution = func(scale=1.0/serviceRate)
-        serviceTimeDistribution.random_state = np.random.RandomState(seed=i*3+21956)
+        serviceTimeDistribution.random_state = np.random.RandomState(seed=i*3+45567)
 
     else:
         serviceTimeDistribution = None
 
     addServer(at=0.0, serviceTimeDistribution=serviceTimeDistribution)
 
+
+changeMC(at=0.0, newMC=100000)
 setRate(at = 0, rate = arrivalRateFrac*serviceRate*nbrServers)
 
 
