07d6b54ba6ac6ffc74a2eb07d9e128089736cc7e
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-02-11 17:54:48 +0100

    Better time optimized

--- sim_optimal_clone.py
+++ sim_optimal_clone.py
@@ -3,7 +3,7 @@ import numpy as np
 from multiprocessing import Pool
 
 # Parameters
-MC_SIMS = 20
+MC_SIMS = 5
 SERVERS = range(1, 13)
 LAMBDA_FRAC = np.arange(0.05, 0.96, 0.01)
 PROCESSES = 24
@@ -14,11 +14,14 @@ simulations = []
 count = 0
 for k in range(MC_SIMS):
     for servers in SERVERS:
-        for frac in LAMBDA_FRAC:
-            count += 1
-            simulations.append("./simulator.py  --lb random --scenario scenarios/clone-sim_optimal_clone.py --cloning 1 --nbrClones {} \
-                --printout 0 --printRespTime 0 --dist SXmodel --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers {} \
-                --setSeed {} --outdir result/s{}_af{}/sim{}".format(servers, frac, servers, count*2+37, servers, frac, k))
+        for i, frac in enumerate(LAMBDA_FRAC):
+            if frac > 0.7 and servers > 5:
+                continue
+            else:
+                count += 1
+                simulations.append("./simulator.py  --lb random --scenario scenarios/clone-sim_optimal_clone.py --cloning 1 --nbrClones {} \
+                    --printout 0 --printRespTime 0 --dist SXmodel --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers {} \
+                    --setSeed {} --outdir result/s{}_af{}/sim{}".format(servers, frac, servers, count*10 + 123456, servers, i, k))
 
 # Run the simulation
 pool = Pool(processes=PROCESSES)
