5ffa66c24d45d84e36b1abf4c18e645fb5a6ee2a
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-05-23 16:59:25 +0200

    Final change

--- sim_clone_SQF.py
+++ sim_clone_SQF.py
@@ -3,15 +3,16 @@ import numpy as np
 from multiprocessing import Pool
 
 # Parameters
-MC_SIMS = [1]#range(0, 20)
-LAMBDA_FRAC = [0.55] #np.arange(0.1, 0.75, 0.05) #[0.1, 0.2 0.3, 0.5, 0.7]
-CLONES = [6] #[2, 3, 4, 6]
-PROCESSES = 1
+MC_SIMS = range(0, 20)
+LAMBDA_FRAC = np.arange(0.1, 0.75, 0.05) #[0.1, 0.2 0.3, 0.5, 0.7]
+CLONES = [2, 3, 4, 6]
+PROCESSES = 24
 MAXRUNTIME = 1000
 
 # Add simulation commands as strings
 simulations = []
 
+buffer = 0.01 # Because of greater than
 count = 0
 for sim in MC_SIMS:
     for i, frac in enumerate(LAMBDA_FRAC):
@@ -19,17 +20,18 @@ for sim in MC_SIMS:
         for clones in CLONES:
             ###
 
-            count += 1
-            simulations.append("./simulator.py  --lb cluster-SQF --scenario scenarios/clone-PS.py --cloning 1 --nbrClones {} \
-                --printout 0 --printRespTime 0 --dist SXmodel --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-                --setSeed {} --maxRunTime {} --outdir result/clusterSQF-PS/c{}_af{}/sim{} \
-                ".format(clones, frac, count*10 + 123456, MAXRUNTIME, clones, i, sim))
+            if not ((clones == 6 and frac > 0.55+buffer) or (clones == 4 and frac > 0.65+buffer)):
+                count += 1
+                simulations.append("./simulator.py  --lb cluster-SQF --scenario scenarios/clone-PS.py --cloning 1 --nbrClones {} \
+                    --printout 0 --printRespTime 0 --dist SXmodel --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+                    --setSeed {} --maxRunTime {} --outdir result/clusterSQF-PS-dolly/c{}_af{}/sim{} \
+                    ".format(clones, frac, count*10 + 123456, MAXRUNTIME, clones, i, sim))
 
-            count += 1
-            simulations.append("./simulator.py  --lb clone-SQF --scenario scenarios/clone-PS.py --cloning 1 --nbrClones {} \
-                --printout 0 --printRespTime 0 --dist SXmodel --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-                --setSeed {} --maxRunTime {} --outdir result/SQF-PS/c{}_af{}/sim{} \
-                ".format(clones, frac, count*10 + 123456, MAXRUNTIME, clones, i, sim))
+                count += 1
+                simulations.append("./simulator.py  --lb clone-SQF --scenario scenarios/clone-PS.py --cloning 1 --nbrClones {} \
+                    --printout 0 --printRespTime 0 --dist SXmodel --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+                    --setSeed {} --maxRunTime {} --outdir result/SQF-PS-dolly/c{}_af{}/sim{} \
+                    ".format(clones, frac, count*10 + 123456, MAXRUNTIME, clones, i, sim))
 
 
 
