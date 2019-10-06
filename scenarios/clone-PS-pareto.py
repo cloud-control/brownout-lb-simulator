7e3b3c6571b8b48d788de3a123266af548812890
Tommi Nylander <tommi.nylander@control.lth.se>
2019-10-06 14:29:21 +0200

    Prepared for pareto sync vs nonsync simulations.

---
+++ scenarios/clone-PS-pareto.py
@@ -0,0 +1,28 @@
+import scipy.stats
+import numpy as np
+from base.numerical_dist import NumericalDistribution
+
+nbrServers = nbrOfServers #12
+seed = setSeed
+for i in range(0, nbrServers):
+
+    if dist == "fromPath":
+        serviceTimeDistribution = NumericalDistribution(cdf_location=distpath, seed=i*3+1852441)
+    elif dist != "SXmodel":
+        try:
+            func = getattr(scipy.stats, dist)
+        except:
+            raise ValueError("Distribution could not load, does it exist in scipy.stats?")
+        serviceTimeDistribution = func(scale=0.6, b=2.5)
+        serviceTimeDistribution.random_state = np.random.RandomState(seed=setSeed+i*3+45567)
+
+    else:
+        serviceTimeDistribution = None
+
+    addServer(at=0.0, serviceTimeDistribution=serviceTimeDistribution, meanStartupDelay=0.00,meanCancellationDelay=0.00)
+
+
+changeMC(at=0.0, newMC=10000)
+setRate(at = 0, rate = arrivalRateFrac*serviceRate*nbrServers)
+endOfSimulation(at = 1000000/(arrivalRateFrac*nbrServers))
+
--- sim_sync_vs_nonsync_icpe.py
+++ sim_sync_vs_nonsync_icpe.py
@@ -4,8 +4,8 @@ from multiprocessing import Pool
 
 # Parameters
 MC_SIMS = range(0, 20)
-LAMBDA_FRAC_2 = [0.2000, 0.5999, 0.9998, 1.3997, 1.7996]
-LAMBDA_FRAC_4 = [0.3997, 1.1990, 1.9984, 2.7977, 3.5971]
+LAMBDA_FRAC_2 = [0.0667, 0.2000, 0.3333, 0.4666, 0.6000]
+LAMBDA_FRAC_4 = [0.0375, 0.1125, 0.1875, 0.2625, 0.3375]
 
 PROCESSES = 24
 MAXRUNTIME = 5000
@@ -18,27 +18,27 @@ for sim in MC_SIMS:
     for i, frac in enumerate(LAMBDA_FRAC_2):
 
         count += 1
-        simulations.append("./simulator.py  --lb cluster-SQF --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/clusterSQF-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb cluster-SQF --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/clusterSQF-PS/c{}_af{}/sim{} \
             ".format(2, frac, count*100 + 123456, MAXRUNTIME, 2, i, sim))
 
         count += 1
-        simulations.append("./simulator.py  --lb cluster-random --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/clusterRandom-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb cluster-random --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/clusterRandom-PS/c{}_af{}/sim{} \
             ".format(2, frac, count*100 + 123456, MAXRUNTIME, 2, i, sim))
 
         count += 1
-        simulations.append("./simulator.py  --lb clone-SQF --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/SQF-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb clone-SQF --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/SQF-PS/c{}_af{}/sim{} \
             ".format(2, frac, count*100 + 123456, MAXRUNTIME, 2, i, sim))
 
         count += 1
-        simulations.append("./simulator.py  --lb clone-random --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/Random-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb clone-random --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/Random-PS/c{}_af{}/sim{} \
             ".format(2, frac, count*100 + 123456, MAXRUNTIME, 2, i, sim))
 
 
@@ -47,27 +47,27 @@ for sim in MC_SIMS:
     for i, frac in enumerate(LAMBDA_FRAC_4):
 
         count += 1
-        simulations.append("./simulator.py  --lb cluster-SQF --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/clusterSQF-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb cluster-SQF --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/clusterSQF-PS/c{}_af{}/sim{} \
             ".format(4, frac, count*100 + 123456, MAXRUNTIME, 4, i, sim))
 
         count += 1
-        simulations.append("./simulator.py  --lb cluster-random --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/clusterRandom-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb cluster-random --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/clusterRandom-PS/c{}_af{}/sim{} \
             ".format(4, frac, count*100 + 123456, MAXRUNTIME, 4, i, sim))
 
         count += 1
-        simulations.append("./simulator.py  --lb clone-SQF --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/SQF-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb clone-SQF --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/SQF-PS/c{}_af{}/sim{} \
             ".format(4, frac, count*100 + 123456, MAXRUNTIME, 4, i, sim))
 
         count += 1
-        simulations.append("./simulator.py  --lb clone-random --scenario scenarios/clone-PS-weibull.py --cloning 1 --nbrClones {} \
-            --printout 0 --printRespTime 0 --dist weibull_min --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
-            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_weibull/Random-PS/c{}_af{}/sim{} \
+        simulations.append("./simulator.py  --lb clone-random --scenario scenarios/clone-PS-pareto.py --cloning 1 --nbrClones {} \
+            --printout 0 --printRespTime 0 --dist pareto --serviceRate 1.0 --arrivalRateFrac {} --nbrOfServers 12 \
+            --setSeed {} --maxRunTime {} --outdir result/sync_vs_nonsync_pareto/Random-PS/c{}_af{}/sim{} \
             ".format(4, frac, count*100 + 123456, MAXRUNTIME, 4, i, sim))
 
 
