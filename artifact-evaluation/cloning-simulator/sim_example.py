ab224154be6d8938356a6a9e25fe9439c814b4cc
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2019-12-19 17:57:56 +0100

    Add index.html and small changes to example script

--- artifact-evaluation/cloning-simulator/sim_example.py
+++ artifact-evaluation/cloning-simulator/sim_example.py
@@ -16,8 +16,8 @@ for k in MC_SIMS:
             --setSeed {} --outdir result_example/random/sim{}".format(count*10 + 123456, k))
 
         simulations.append("./simulator.py  --lb clone-SQF --scenario scenarios/clone-example.py --cloning 1 --nbrClones 3 \
-            --printout 0 --printRespTime 1 --dist fromPath --path dists/mintest-v2.csv  --serviceRate 1.0 --uniformArrivals 1 \
-            --arrivalRateFrac 0.5 --nbrOfServers 9 --setSeed {} --outdir result_example/sqf/sim{}".format(count*10 + 123456, k))
+            --printout 0 --printRespTime 1 --dist SXmodel --serviceRate 1.0 --uniformArrivals 1 --arrivalRateFrac 0.5 --nbrOfServers 9 \
+            --setSeed {} --outdir result_example/sqf/sim{}".format(count*10 + 123456, k))
 
 
 # Each simulation can take a while to execute, but runs in a single core. Since they are independent, parallel
